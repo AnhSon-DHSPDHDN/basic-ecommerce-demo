8cc5882972f841b563affffe82ac37546de45ec7
sontva-2630 <tran.van.anh.son@sun-asterisk.com>
2025-03-11 22:26:28 +0700

    add navigate header

--- src/components/HeaderComponent/index.tsx
+++ src/components/HeaderComponent/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { ShoppingCart } from "lucide-react";
+import { Link, useNavigate } from "react-router";
 
 const HeaderComponent: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleNavigateToCartPage = () => {
+    navigate("/cart");
+  };
+
   return (
     <header className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white py-4 px-6 flex justify-between items-center shadow-lg rounded-b-lg">
-      <h1 className="text-2xl font-bold tracking-wide">My Shop</h1>
+      <Link className="text-2xl font-bold tracking-wide" to={"/home"}>
+        My Shop
+      </Link>
 
-      <div className="relative flex items-center">
+      <div
+        className="relative flex items-center"
+        onClick={handleNavigateToCartPage}
+      >
         <ShoppingCart className="w-7 h-7 cursor-pointer hover:text-gray-300 transition duration-300" />
 
         <span className="absolute -top-2 -right-2 bg-red-500 text-xs rounded-full w-5 h-5 flex items-center justify-center font-semibold shadow-md">
