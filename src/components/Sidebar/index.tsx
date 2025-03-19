e252a11aa271d9ef919b094fba74b96afc570562
sontva-2630 <tran.van.anh.son@sun-asterisk.com>
2025-03-19 21:55:52 +0700

    show brands

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -15,18 +15,12 @@ const Sidebar: React.FC = () => {
     <aside className="col-span-1 self-start bg-white shadow-lg p-4 rounded-lg sticky top-0 overflow-y-auto">
       <h2 className="text-xl font-semibold mb-4">Filter by Brand</h2>
       <ul className="space-y-2 mb-4">
-        <li>
-          <input type="checkbox" id="brand1" />{" "}
-          <label htmlFor="brand1">Brand A</label>
-        </li>
-        <li>
-          <input type="checkbox" id="brand2" />{" "}
-          <label htmlFor="brand2">Brand B</label>
-        </li>
-        <li>
-          <input type="checkbox" id="brand3" />{" "}
-          <label htmlFor="brand3">Brand C</label>
-        </li>
+        {brands.map((brand) => (
+          <li>
+            <input type="checkbox" id={`${brand.id}`} value={brand.id} />{" "}
+            <label htmlFor={`${brand.id}`}>{brand.brandName}</label>
+          </li>
+        ))}
       </ul>
 
       <h2 className="text-xl font-semibold mb-2">Price Range</h2>
