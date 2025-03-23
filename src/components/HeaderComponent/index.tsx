6e98bbff7c9668d00ebf619d9346961e6acfa140
sontva-2630 <tran.van.anh.son@sun-asterisk.com>
2025-03-23 18:47:50 +0700

    clear filter

--- src/components/HeaderComponent/index.tsx
+++ src/components/HeaderComponent/index.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import { ShoppingCart } from "lucide-react";
 import { Link, useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../redux/store/store";
+import { clearAllFilter } from "../../redux/features/products/productSlice";
 
 const HeaderComponent: React.FC = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch<AppDispatch>();
 
   const handleNavigateToCartPage = () => {
     navigate("/cart");
   };
 
+  const handleClearFilter = () => {
+    dispatch(clearAllFilter());
+  };
+
   return (
     <header className="bg-gradient-to-r from-blue-500 to-indigo-600 text-white py-4 px-6 flex justify-between items-center shadow-lg rounded-b-lg">
-      <Link className="text-2xl font-bold tracking-wide" to={"/home"}>
+      <Link
+        className="text-2xl font-bold tracking-wide"
+        to={"/home"}
+        onClick={handleClearFilter}
+      >
         My Shop
       </Link>
 
